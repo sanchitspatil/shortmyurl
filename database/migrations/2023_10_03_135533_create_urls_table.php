d33b43362a60dd41f4b4678cac6af5728b037eeb
Sanchit <sanchit.patil@healthierbusiness.co.uk>
2023-10-03 15:57:23 +0100

    urls & url_logs migration added

---
+++ database/migrations/2023_10_03_135533_create_urls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->string('keyword');
+            $table->text('url')->nullable();
+            $table->string('title')->nullable();
+            $table->string('ip_addrs')->nullable();
+            $table->string('http_code')->nullable();
+            $table->integer('clicks')->default(0);
+            $table->text('notes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('urls');
+    }
+};
